f0a4b89c039be7fd0511eb6aef4c6fcb8cde4815
张昊 <dzxx0563zh@126.com>
2015-08-12 00:10:47 +0800

    start backend

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -1,9 +1,1 @@
-"use strict";
-$(window).load(function () {	
-	$(".tag").get(0)
-		.on("mouseenter", function (event) {
-			console.log(event.target);
-			$(event.target).css({y: 30});
-		
-		});	
-});
+// 动画暂时不做
