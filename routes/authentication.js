38cf8f859a0665f1e84d1a55802f08a69fe0c3df
Summerlve <dzxx0563zh@126.com>
2015-10-29 21:50:03 +0800

    auth rewrite

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,6 +3,7 @@ let router = require("koa-router")();
 let views = require("co-views");
 let parse = require("co-body");
 let User = require("../models/User");
+let Group = require("../models/Group");
 let MD5 = require("md5");
 let jwt = require("jsonwebtoken");
 let moment = require("moment");
@@ -31,49 +32,50 @@ router
 		let body = yield parse.form(this);
 
 		let user = yield User.find({
-			attributes: ["id", "username", "avatar", "pen_name"],
+			attributes: ["id", "username", "avatar", "pen_name", "group_id"],
 			where: {
 				username: body.username,
 				password: MD5(body.password)
 			}
 		});
 
-		if (user !== null) {
-			// set the http header field
-			this.set("Pragma", "no-cache");
-			this.set("Cache-Control", "no-store");
-
-			// expires time is 7 days
-			let days = 7;
-			let seconds = days * 24 * 60 * 60;
-			// this is unix timestamp with utc time
-			let expires = moment().utcOffset(8).add(seconds, "seconds").valueOf();
-
-			// create token
-			let token = jwt.sign({
-				iss: user.id,
-				exp: expires
-			}, cert);
-
-			// store the token into redis , and set the expires time
-			redisClient.set(token, "");
-			redisClient.expire(token, seconds);
-
-			// return json
-			this.body = {
-				token: token,
-				expires: expires,
-				user: user
-			};
-		}
-		else {
+		if (user === null) {
 			this.status = 401;
 			this.body = {
 				statusCode: 401,
 				reasonPhrase: "Unauthorized",
 				description: "username or password is not correct"
-			}
+			};
+
+			return ;
 		}
+
+		// set the http header field
+		this.set("Pragma", "no-cache");
+		this.set("Cache-Control", "no-store");
+
+		// expires time is 7 days
+		let days = 7;
+		let seconds = days * 24 * 60 * 60;
+		// this is unix timestamp with utc time
+		let expires = moment().utcOffset(8).add(seconds, "seconds").valueOf();
+
+		// create token
+		let token = jwt.sign({
+			iss: user.id,
+			exp: expires
+		}, cert);
+
+		// store the token into redis , and set the expires time
+		redisClient.set(token, "");
+		redisClient.expire(token, seconds);
+
+		// return json
+		this.body = {
+			token: token,
+			expires: expires,
+			user: user
+		};
 	});
 
 // 注销功能：删除存储在redis里面的token，如果删除成功则通知前端。
