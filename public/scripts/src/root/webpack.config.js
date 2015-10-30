e02397814896a2cbded46b79919999f2735e9295
Summerlve <dzxx0563zh@126.com>
2015-10-30 10:04:28 +0800

    script change

---
+++ public/scripts/src/root/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./main.js",
+    output: {
+        filename: "../build/root.js"
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.vue$/,
+            loader: "vue"
+          }
+        ]
+    }
+};
--- public/scripts/utils/test.js
+++
@@ -1,3 +0,0 @@
-console.log(
-    equal(
-        inc(1), dec(3)))
