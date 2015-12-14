e5a94d5fdde0f6b9cf4f8c17d1701151ab48f099
Summerlve <dzxx0563zh@126.com>
2015-12-14 21:38:40 +0800

    start test

--- test/users.test.js
+++ test/users.test.js
@@ -3,39 +3,35 @@
 const request = require("request");
 const configs = require("./configs.json");
 const assert = require("assert");
+const util = require("util");
+const prefix = "/user";
+const protocol = configs.app.protocol;
+const host = configs.app.host;
+const port = configs.app.port;
 
 describe("get users info from configs.json", function () {
-    it("get root", function () {
-        assert.deepStrictEqual(configs.users.root.length, 1, "root users's length must be one");
+    it("get root user", function (done) {
+        assert.deepStrictEqual(configs.users.root.length >= 1, true, "root users's length must >= 1");
+        done();
+    });
+
+    it("get token must be ok", function (done) {
+        let username = configs.users.root[0].username;
+        let password = configs.users.root[0].password;
+        console.log(username, password);
+        request({
+            method: "POST",
+            url: util.format("%s://%s:%s%s", protocol, host, port, "/authentications"),
+            form: {
+                username: username,
+                password: password
+            }, function (error, response, body) {
+                assert.strictEqual(error, null);
+                assert.strictEqual(response.statusCode, 200);
+                let token = response.token;
+                console.log(token);
+                done();
+            }
+        });
     });
 });
-
-
-
-
-// request
-//     .post(
-//         {
-//             url: createUser,
-//             headers: {
-//                 Authorization: "jwt " + token,
-//                 Accept: "application/json"
-//             },
-//             form: {
-//                 username: "author5@test.com",
-//                 password: "123456",
-//                 penName: "authorTest-5",
-//                 avatar: null,
-//                 introduce: null,
-//                 groupId: 3
-//             }
-//         },
-//         function (err, httpResponse, body) {
-//             if (err) {
-//                 console.error(err);
-//                 return ;
-//             }
-//
-//             console.log(body);
-//         }
-//     );
