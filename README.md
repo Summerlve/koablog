8fd44e7a1e0f3866ea2692f6fad75131fb6b7b5a
Summerlve <dzxx0563zh@126.com>
2015-12-17 20:42:09 +0800

    some change

--- README.md
+++ README.md
@@ -23,4 +23,26 @@
 
 /tags
 
-# Error Code Design
+# Error Code
+<table width="100%" border="1">
+	<tr>
+		<td colspan="4">/users</td>
+	</tr>
+	<tr>
+		<td>/users</td>
+		<td colspan="3">/users</td>
+	</tr>
+</table>
+
+<table border="1">
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+  <tr>
+    <td colspan="2">January</td>
+  </tr>
+  <tr>
+    <td colspan="2">February</td>
+  </tr>
+</table>
--- test/users.test.js
+++ test/users.test.js
@@ -5,24 +5,28 @@ const configs = require("./configs.json");
 const assert = require("assert");
 const util = require("util");
 const prefix = "/authors";
+const auth = "/authentications";
 const protocol = configs.app.protocol;
 const host = configs.app.host;
 const port = configs.app.port;
-let token = "";
 
 describe("test the /authors", function () {
+    let token;
+    let userId;
+    let username;
+    let password;
+
     it("get root user must be ok", function (done) {
         assert.deepStrictEqual(configs.users.root.length >= 1, true, "root users's length must >= 1");
+        username = configs.users.root[0].username;
+        password = configs.users.root[0].password;
         done();
     });
 
-    it("get token must be ok", function (done) {
-        let username = configs.users.root[0].username;
-        let password = configs.users.root[0].password;
-
+    it("log in must be ok", function (done) {
         request({
             method: "POST",
-            url: util.format("%s://%s:%s%s", protocol, host, port, "/authentications"),
+            url: util.format("%s://%s:%s%s", protocol, host, port, auth),
             form: {
                 username: username,
                 password: password
@@ -30,9 +34,34 @@ describe("test the /authors", function () {
         }, function (error, response, body) {
             assert.strictEqual(error, null);
             assert.strictEqual(response.statusCode, 200);
-
             token = JSON.parse(body).token;
             assert.strictEqual(typeof token, "string");
+            done();
+        });
+    });
+
+    it("log out with right token must be ok", function (done) {
+        request({
+            method: "DELETE",
+            url: util.format("%s://%s:%s%s", protocol, host, port, auth),
+            headers: {
+                "Authorization": "jwt " + token
+            }
+        }, function (error, response, body) {
+            assert.strictEqual(error, null);
+            assert.strictEqual(response.statusCode, 200);
+            done();
+        });
+    });
+
+    it("log out with none token must be failed", function (done) {
+        request({
+            method: "DELETE",
+            url: util.format("%s://%s:%s%s", protocol, host, port, auth),
+        }, function (error, response, body) {
+            assert.strictEqual(error, null);
+            assert.strictEqual(response.statusCode, 401);
+            assert.strictEqual(JSON.parse(body).errorCode, 1000);
             done();
         });
     });
@@ -57,16 +86,15 @@ describe("test the /authors", function () {
         }, function(error, response, body) {
             assert.strictEqual(error, null);
             assert.strictEqual(response.statusCode, 200);
-
             done();
         });
     });
 
-    it("create a new user must be ok", function (done) {
+    it("create a user with right token must be ok", function (done) {
         request({
             method: "POST",
             form: {
-
+                username
             }
         }, function (error, response, body) {
             assert.strictEqual(error, null);
