4bb230239dde41c4b4340a541c0cd343cef7f8dc
Summerlve <dzxx0563zh@126.com>
2015-12-26 11:41:25 +0800

    some change

--- test/files.test.js
+++ test/files.test.js
@@ -1,13 +1,12 @@
 "use strict";
-// test router '/groups'
+// test router '/files'
 const request = require("request");
 const configs = require("./configs.json");
 const assert = require("assert");
 const util = require("util");
 
-
 // configs
-const routerPrefix = "/groups";
+const routerPrefix = "/files";
 const authentications = "/authentications";
 const protocol = configs.app.protocol;
 const host = configs.app.host;
--- test/tags.test.js
+++ test/tags.test.js
@@ -4,8 +4,6 @@ const request = require("request");
 const configs = require("./configs.json");
 const assert = require("assert");
 const util = require("util");
-const jwt = require("jsonwebtoken");
-const moment = require("moment");
 
 // configs
 const routerPrefix = "/tags";
@@ -13,7 +11,6 @@ const authentications = "/authentications";
 const protocol = configs.app.protocol;
 const host = configs.app.host;
 const port = configs.app.port;
-const cert = configs.jwt.cert;
 
 describe("Test the /tags", function () {
     // group of root
